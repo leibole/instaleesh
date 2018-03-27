303f1609dba7eaab691e6c986b42022249d64c4a
leibole <ido.leibole@gmail.com>
2018-03-27 21:54:48 +0300

    add categories, add versioned bootstrap

--- src/SinglePhoto.js
+++ src/SinglePhoto.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { Col } from 'react-bootstrap';
-import { Card, CardMedia, CardTitle } from 'material-ui/Card';
-import reactDOM from 'react-dom';
-import { CommentInput, SinglePhotoComments } from './Comments';
-import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
-import FileDownload from 'material-ui/svg-icons/file/file-download';
-import './SinglePhoto.css';
+import React, { Component } from "react";
+import { Col } from "react-bootstrap";
+import { Card, CardMedia, CardTitle } from "material-ui/Card";
+import reactDOM from "react-dom";
+import { CommentInput, SinglePhotoComments } from "./Comments";
+import CommunicationChatBubble from "material-ui/svg-icons/communication/chat-bubble";
+import FileDownload from "material-ui/svg-icons/file/file-download";
+import "./SinglePhoto.css";
 
 class SinglePhoto extends Component {
   constructor(props) {
@@ -24,42 +24,61 @@ class SinglePhoto extends Component {
         lgOffset={this.props.singleView ? 3 : 0}
         xs={this.props.singleView ? 10 : 4}
         xsOffset={this.props.singleView ? 1 : 0}
-        className="mobile-padding" >
+        className="mobile-padding"
+      >
         <Card
-          ref={(cardRef) => { this.cardRef = cardRef }}>
-          <CardMedia
-            style={{ cursor: this.props.singleView ? '' : 'pointer' }} >
-            <a href={"//res.cloudinary.com/instaleesh/image/upload/" + this.props.image.public_id} download>
-            <FileDownload 
-            style={{ 
-              height: '30px',
-              width: '30px',
-              position: 'absolute',
-              top: 0, 
-              right: 0,
-              color: 'black',
-              opacity: 0.5
-            }} />
+          ref={cardRef => {
+            this.cardRef = cardRef;
+          }}
+        >
+          <CardMedia style={{ cursor: this.props.singleView ? "" : "pointer" }}>
+            <a
+              href={
+                "//res.cloudinary.com/instaleesh/image/upload/" +
+                this.props.image.public_id
+              }
+              download
+            >
+              <FileDownload
+                style={{
+                  height: "30px",
+                  width: "30px",
+                  position: "absolute",
+                  top: 0,
+                  right: 0,
+                  color: "black",
+                  opacity: 0.5
+                }}
+              />
             </a>
             <img
-              src={"//res.cloudinary.com/instaleesh/image/upload/q_60/" + this.props.image.public_id + ".jpg"}
+              src={
+                "//res.cloudinary.com/instaleesh/image/upload/q_60/" +
+                this.props.image.public_id +
+                ".jpg"
+              }
               alt="Great furniture"
               onClick={this.imageClicked.bind(this)}
-            >
-            </img>
+            />
           </CardMedia>
           <Col xsHidden={!this.props.singleView}>
             <CardTitle>
-              <CommentInput imageId={this.props.image.public_id} user={this.props.user} />
+              <CommentInput
+                imageId={this.props.image.public_id}
+                user={this.props.user}
+              />
             </CardTitle>
             <SinglePhotoComments
               imageId={this.props.image.public_id}
-              imageAddCallback={this.updateNumberOfImages} />
+              imageAddCallback={this.updateNumberOfImages}
+            />
           </Col>
-          <Col xsHidden={this.props.singleView} mdHidden={true} lgHidden={true} >
-            <CardTitle style={{ padding: '5px' }}>
-              <CommunicationChatBubble style={{ height: '20px', width: '20px' }} />
-              <span style={{ marginLeft: '5px', verticalAlign: 'top' }}>
+          <Col xsHidden={this.props.singleView} mdHidden={true} lgHidden={true}>
+            <CardTitle style={{ padding: "5px" }}>
+              <CommunicationChatBubble
+                style={{ height: "20px", width: "20px" }}
+              />
+              <span style={{ marginLeft: "5px", verticalAlign: "top" }}>
                 {this.state.numberOfImages}
               </span>
             </CardTitle>
@@ -77,6 +96,6 @@ class SinglePhoto extends Component {
     var imageDOMElement = reactDOM.findDOMNode(this.cardRef);
     this.props.onImageClick(imageDOMElement);
   }
-};
+}
 
 export default SinglePhoto;
--- src/TopBar.js
+++ src/TopBar.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { AppBar } from 'material-ui';
-import NavigationArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
-import IconButton from 'material-ui/IconButton';
-import { Image } from 'cloudinary-react';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
-import FlatButton from 'material-ui/FlatButton';
-import Toggle from 'material-ui/Toggle';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import Menu from 'material-ui/svg-icons/navigation/menu';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import React, { Component } from "react";
+import { AppBar } from "material-ui";
+import NavigationArrowBack from "material-ui/svg-icons/navigation/arrow-back";
+import IconButton from "material-ui/IconButton";
+import { Image } from "cloudinary-react";
+import IconMenu from "material-ui/IconMenu";
+import MenuItem from "material-ui/MenuItem";
+import FlatButton from "material-ui/FlatButton";
+import Toggle from "material-ui/Toggle";
+import MoreVertIcon from "material-ui/svg-icons/navigation/more-vert";
+import Menu from "material-ui/svg-icons/navigation/menu";
+import NavigationClose from "material-ui/svg-icons/navigation/close";
 
 class TopBar extends Component {
   constructor(props) {
@@ -17,7 +17,7 @@ class TopBar extends Component {
     this.state = {
       images: [],
       singleView: false,
-      subject: ''
+      subject: ""
     };
     this.handleChangeIssue = this.handleChangeIssue.bind(this);
   }
@@ -26,30 +26,44 @@ class TopBar extends Component {
     return (
       <AppBar
         zDepth={4}
-        iconStyleLeft={{ margin: 'auto' }}
+        iconStyleLeft={{ margin: "auto" }}
         iconElementLeft={
           <div>
             <IconButton
               onClick={this.props.backCallback}
-              style={{ width: 70, height: 70, color: 'black', display: this.props.singleView ? '' : 'none' }}
-              iconStyle={{ width: 70, height: 70, padding: 14, color: 'black' }}>
+              style={{
+                width: 70,
+                height: 70,
+                color: "black",
+                display: this.props.singleView ? "" : "none"
+              }}
+              iconStyle={{ width: 70, height: 70, padding: 14, color: "black" }}
+            >
               <NavigationArrowBack />
             </IconButton>
             <IconMenu
               iconButtonElement={
                 <IconButton
-                  style={{ width: 70, height: 70, color: 'black' }}
-                  iconStyle={{ width: 70, height: 70, padding: 14, color: 'black' }}>
-                  <Menu style={{ fill: 'black' }} />
+                  style={{ width: 70, height: 70, color: "black" }}
+                  iconStyle={{
+                    width: 70,
+                    height: 70,
+                    padding: 14,
+                    color: "black"
+                  }}
+                >
+                  <Menu style={{ fill: "black" }} />
                 </IconButton>
               }
-              targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-              anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+              targetOrigin={{ horizontal: "right", vertical: "top" }}
+              anchorOrigin={{ horizontal: "right", vertical: "top" }}
               onChange={this.handleChangeIssue}
               value={this.state.subject}
             >
               <MenuItem value="" primaryText="Home" />
               <MenuItem value="kitchen" primaryText="Kitchen" />
+              <MenuItem value="plans" primaryText="Plans" />
+              <MenuItem value="models" primaryText="3D models" />
               <MenuItem value="livingroom" primaryText="Living Room" />
               <MenuItem value="bedroom" primaryText="Bedroom" />
               <MenuItem value="children" primaryText="Chidren's Room" />
@@ -60,21 +74,22 @@ class TopBar extends Component {
           </div>
         }
         style={{
-          backgroundColor: '#FFFFFF',
-          primaryText: 'black'
-        }} >
+          backgroundColor: "#FFFFFF",
+          primaryText: "black"
+        }}
+      >
         <Image
-          cloudName={'instaleesh'}
-          publicId={'title_logo_mchxjq'}
+          cloudName={"instaleesh"}
+          publicId={"title_logo_mchxjq"}
           height={100}
         />
       </AppBar>
-    )
+    );
   }
 
   handleChangeIssue = (event, value) => {
     this.props.changeSubject(value);
-    this.setState({ subject: value })
+    this.setState({ subject: value });
   };
 }
 
